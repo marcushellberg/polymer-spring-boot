57831d740bc4e8b96f3b56fcfb6416ad75fd2858
Marcus Hellberg <marcus@vaadin.com>
2016-02-01 11:18:16 -0800

    imported project from Spring initalizr

---
+++ src/main/java/org/vaadin/marcus/PolymerDemoApplication.java
@@ -0,0 +1,12 @@
+package org.vaadin.marcus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolymerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolymerDemoApplication.class, args);
+	}
+}
