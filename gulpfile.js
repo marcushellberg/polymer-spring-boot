60f3fb2f5ac619ecd90cf1e735a8c41bd3c52a10
Marcus Hellberg <marcus@vaadin.com>
2016-02-01 16:13:39 -0800

    call npm, bower, gulp from maven build. added production profile to create vulcanized import

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+'use strict';
+const SRC_DIR = 'src/main/resources/static';
+const DEST_DIR = 'target/classes/static';
+const gulp = require('gulp');
+const vulcanize = require('gulp-vulcanize');
+const autoprefixer = require('gulp-autoprefixer');
+
+
+gulp.task('vulcanize', function(){
+  return gulp
+    .src(SRC_DIR + '/elements.html')
+    .pipe(vulcanize({
+      inlineScripts: true,
+      inlineCss: true
+    }))
+    .pipe(gulp.dest(DEST_DIR));
+});
+
+gulp.task('copy-resources', function(){
+  return gulp.src(SRC_DIR+'/bower_components/webcomponentsjs/**/*')
+    .pipe(gulp.dest(DEST_DIR+'/bower_components/webcomponentsjs'));
+});
+
+gulp.task('autoprefix', function(){
+  return gulp.src(SRC_DIR + '/styles/**/*.css')
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(gulp.dest(DEST_DIR+'/styles'));
+});
+
+gulp.task('default', ['vulcanize', 'copy-resources', 'autoprefix']);
